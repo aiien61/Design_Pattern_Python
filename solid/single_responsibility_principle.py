caee9c97111e4203d1a6a8c23886719dd1e41d4f
aiien61 <aiien61will@gmail.com>
2024-05-30 03:32:23 +0800

    more description of single responsibility principle

--- solid/single_responsibility_principle.py
+++ solid/single_responsibility_principle.py
@@ -1,3 +1,7 @@
+"""Single Responsibility Principle 單一職責原則 (= Separation of Concerns 關注點分離): 
+A class should have only one reason to change, meaning it should have only one job or responsibility.
+"""
+
 class Journal:
     def __init__(self):
         self.entries = []
@@ -8,7 +12,8 @@ class Journal:
         self.entries.append(f'{self.count}: {text}')
     
     def remove_entry(self, position: int):
-        del self.entries[position]
+        if 0 <= position < len(self.entries):
+            del self.entries[position]
 
     def __str__(self):
         return '\n'.join(self.entries)
@@ -31,25 +36,84 @@ class PersistenceManager:
         file.write(str(journal))
         file.close()
 
+    @staticmethod
+    def load_from_file(filename: str):
+        with open(filename, 'r') as file:
+            contents = file.read()
+            journal = Journal()
+            for entry in contents.split('\n'):
+                journal.add_entry(':'.join(entry.split(': ')[1:]))
+            return journal
+
+
+"""Consider whether or not the following statement makes sense for every method in a class.
+The <classname> <method> itself
+
+e.g.
+Before the separation of concerns:
+class Automobile:
+    start()
+    stop()
+    change_tires()
+    drive()
+    wash()
+    check_oil()
+    get_oil()
+
+
+The Automobile starts itself (o)
+The Automobile stops itself (o)
+The Automobile changes its tires (x) -> The CarWash washes an automobile itself (o)
+The Automobile drives itself (o)
+The Automobile washes itself (x)
+The Automobile checks oil itself (x)
+The Automobile gets oil itself (o)
+
+After the separation of concerns:
+class Automobile:
+    start()
+    stop()
+    drive()
+    get_oil()
+
+class CarWash:
+    wash()
+
+class Mechanic:
+    change_tires()
+    check_oil()
+"""
 
 if __name__ == '__main__':
-    j = Journal()
-    j.add_entry('I cried today.')
-    j.add_entry('I smiled tonight.')
+    journal = Journal()
+    journal.add_entry("Monday: A fresh start in a new week.")
+    journal.add_entry("Challenge of The Day: Learn Design Patterns.")
+    journal.add_entry("Reminder: Recording the progress.")
 
-    print(f'Journal entries: \n{j}\n')
+
+    print(f'Journal entries: \n{journal}\n')
+    
+    print('\nDelete the second entry:')
+    journal.remove_entry(1)
+    print(f'Journal entries: \n{journal}\n')
 
     '''This operation is weird as it breaks the single responsibility principle
     the Journal instance should not be responsible for persisting its data
     to a file and load a journal from a file.'''
-    # j.save('data/journal.txt')
-    # new_j = j.load('data/journal.txt')
-    # print(f'New journal entries: \n{new_j}\n')
 
-    '''This operation stick to the rule of the single responsibility principle'''
-    p = PersistenceManager()
-    p.save_to_file(j, 'data/journal.txt')
+    # print('\nSave journal to a file')
+    # journal.save('../data/journal.txt')
+
+    # print('\nLoad journal from a file:')
+    # new_journal = journal.load('../data/journal.txt')
+
+    # print(f'New journal entries: \n{new_journal}\n')
+
+    '''This operation sticks to the rule of the single responsibility principle'''
+    print('\nSave journal to a file')
+    PersistenceManager.save_to_file(journal, '../data/journal.txt')
     
-    # verify
-    with open('data/journal.txt', 'r') as file:
-        print(file.read())
+    print('\nLoad journal from a file:')
+    new_journal = PersistenceManager.load_from_file('../data/journal.txt')
+    print(f'New journal entries: \n{new_journal}\n')
+    
